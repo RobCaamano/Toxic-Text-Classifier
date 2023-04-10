8ef234159eb441311e6fa3fe7be96cd3dd2e531b
Rob Caamano <65639885+RobCaamano@users.noreply.github.com>
2023-04-09 23:01:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ demo = """I'm so proud of myself for accomplishing my goals today. #motivation #
 
 text = st.text_area("Input text", demo, height=250)
 
-model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
