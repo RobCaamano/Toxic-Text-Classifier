be5dc38eeeb4a73808eb1f5fb04045975cbc7c01
Rob Caamano <65639885+RobCaamano@users.noreply.github.com>
2023-04-27 18:54:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ model_name = ""
 
 with st.container():
     model_name = st.selectbox(
-        "Select the model you want to use below.",
+        "Select Model",
         ("RobCaamano/toxicity",),
     )
     submit = st.button("Submit", type="primary")
@@ -36,13 +36,19 @@ if submit:
     max_class = max(classes, key=classes.get)
     probability = classes[max_class]
 
-    result_df = pd.DataFrame({
-        'Toxic': ['Yes' if results['toxic'] >= 0.5 else 'No'],
-        'Classification': [max_class],
-        'Probability': [probability]
-    })
+    if results['toxic'] >= 0.5:
+        result_df = pd.DataFrame({
+            'Toxic': ['Yes'],
+            'Toxicity Class': [max_class],
+            'Probability': [probability]
+        })
+    else:
+        result_df = pd.DataFrame({
+            'Toxic': ['No'],
+            'Toxicity Class': 'This text is not toxic',
+        })
 
     st.table(result_df)
 
-    expander = st.expander("Raw output")
+    expander = st.expander("View Raw output")
     expander.write(results)
