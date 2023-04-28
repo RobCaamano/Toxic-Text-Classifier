ebebffc003c56c981fa1be43647cb08ed50314d2
Rob Caamano <65639885+RobCaamano@users.noreply.github.com>
2023-04-28 01:58:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ model_name = ""
 
 model_mapping = {
     "Toxicity": "RobCaamano/toxicity",
-    "Toxicity 2": "RobCaamano/toxicity_distilbert",
     "DistilBERT Base Uncased (SST-2)": "distilbert-base-uncased-finetuned-sst-2-english",
 }
 
@@ -47,7 +46,7 @@ input = tokenizer(text, return_tensors="tf")
 if submit:
     results = dict(d.values() for d in clf(text)[0])
 
-    if model_name in ["RobCaamano/toxicity", "RobCaamano/toxicity_distilbert"]:
+    if model_name == "RobCaamano/toxicity":
         classes = {k: results[k] for k in results.keys() if not k == "toxic"}
 
         max_class = max(classes, key=classes.get)
